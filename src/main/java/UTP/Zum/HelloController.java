75dafc7c8d9806fbc0489e61bfb12358c4e5d8d7
Leonardo <LeonardoEsteban_19@hotmail.com>
2025-10-21 22:51:19 -0500

    Fisrt Test

--- src/main/java/UTP/Zum/HelloController.java
+++ src/main/java/UTP/Zum/HelloController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String saludar(){
-        return "Buenas";
+        return "Buenas como estas";
+        
+        
     }
 }
